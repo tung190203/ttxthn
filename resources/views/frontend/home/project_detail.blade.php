0e4ad64f2ea08e5808a95ccb93bb3ffc051478ca
Nguyen Thanh Tung <tung19022003@gmail.com>
2025-08-07 16:32:39 +0700

    feat:layout detail blade

--- resources/views/frontend/home/project_detail.blade.php
+++ resources/views/frontend/home/project_detail.blade.php
@@ -3,70 +3,70 @@
 @section('content')
     <div class="page__content">
         <!-- main content-->
-        @if($project->layout_id == 1)
-        <section class="pj-banner">
-            <nav>
-                <div class="container">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a class="link-unstyled" href="/"><i
-                                    class="fal fa-home me-2"></i><span>Trang chủ</span></a></li>
-                        <li class="breadcrumb-item active">Danh mục dự án đầu tư</li>
-                    </ol>
-                </div>
-            </nav>
-            <img class="pj-banner__bg" src="{{$project->banner_image ?? 'default-banner.jpg'}}" alt="">
-            <div class="pj-banner__wrapper">
-                <div class="container">
-                    <div class="pj-banner__subtitle">Dự án</div>
-                    <div class="pj-banner__title">{{$project->name}}</div>
-                    <div class="pj-banner__separator"></div>
-                    <div class="pj-banner__desc">{{$project->short_desc ?? ''}}</div>
-                    <div class="pj-banner__icon"><i class="fal fa-arrow-down"></i></div>
-                </div>
-            </div>
-        </section>
-        @elseif($project->layout_id == 2)
-        <section class="pj-banner">
-            <nav>
-                <div class="container">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a class="link-unstyled" href="/"><i
-                                    class="fal fa-home me-2"></i><span>Trang chủ</span></a></li>
-                        <li class="breadcrumb-item active">Danh mục dự án đầu tư</li>
-                    </ol>
-                </div>
-            </nav>
-            <img class="pj-banner__bg" src="{{$project->banner_image ?? 'default-banner.jpg'}}" alt="">
-            <div class="pj-banner__wrapper custom-wrapper">
-                <div class="container">
-                    <div class="pj-banner__subtitle text-end">Dự án</div>
-                    <div class="pj-banner__title text-end">{{$project->name}}</div>
-                    <div class="custom_desc">{{$project->short_desc ?? ''}}</div>
-                </div>
-            </div>
-        </section>
-        @elseif($project->layout_id == 3)
-        <section class="pj-banner">
-            <nav>
-                <div class="container">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a class="link-unstyled" href="/"><i
-                                    class="fal fa-home me-2"></i><span>Trang chủ</span></a></li>
-                        <li class="breadcrumb-item active">Danh mục dự án đầu tư</li>
-                    </ol>
-                </div>
-            </nav>
-            <img class="pj-banner__bg" src="{{$project->banner_image ?? 'default-banner.jpg'}}" alt="">
-            <div class="pj-banner__wrapper custom-wrapper">
-                <div class="container">
-                    <div class="pj-banner__subtitle text-start">Dự án</div>
-                    <div class="pj-banner__title text-start">{{$project->name}}</div>
-                    <div class="d-flex justify-content-end">
-                        <div class="custom_desc">{{$project->short_desc ?? ''}}</div>
+        @if ($project->layout_id == 1)
+            <section class="pj-banner">
+                <nav>
+                    <div class="container">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a class="link-unstyled" href="/"><i
+                                        class="fal fa-home me-2"></i><span>Trang chủ</span></a></li>
+                            <li class="breadcrumb-item active">Danh mục dự án đầu tư</li>
+                        </ol>
+                    </div>
+                </nav>
+                <img class="pj-banner__bg" src="{{ $project->banner_image ?? 'default-banner.jpg' }}" alt="">
+                <div class="pj-banner__wrapper">
+                    <div class="container">
+                        <div class="pj-banner__subtitle">Dự án</div>
+                        <div class="pj-banner__title">{{ $project->name }}</div>
+                        <div class="pj-banner__separator"></div>
+                        <div class="pj-banner__desc">{{ $project->short_desc ?? '' }}</div>
+                        <div class="pj-banner__icon"><i class="fal fa-arrow-down"></i></div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        @elseif($project->layout_id == 2)
+            <section class="pj-banner">
+                <nav>
+                    <div class="container">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a class="link-unstyled" href="/"><i
+                                        class="fal fa-home me-2"></i><span>Trang chủ</span></a></li>
+                            <li class="breadcrumb-item active">Danh mục dự án đầu tư</li>
+                        </ol>
+                    </div>
+                </nav>
+                <img class="pj-banner__bg" src="{{ $project->banner_image ?? 'default-banner.jpg' }}" alt="">
+                <div class="pj-banner__wrapper custom-wrapper">
+                    <div class="container">
+                        <div class="pj-banner__subtitle text-end">Dự án</div>
+                        <div class="pj-banner__title text-end">{{ $project->name }}</div>
+                        <div class="custom_desc">{{ $project->short_desc ?? '' }}</div>
+                    </div>
+                </div>
+            </section>
+        @elseif($project->layout_id == 3)
+            <section class="pj-banner">
+                <nav>
+                    <div class="container">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a class="link-unstyled" href="/"><i
+                                        class="fal fa-home me-2"></i><span>Trang chủ</span></a></li>
+                            <li class="breadcrumb-item active">Danh mục dự án đầu tư</li>
+                        </ol>
+                    </div>
+                </nav>
+                <img class="pj-banner__bg" src="{{ $project->banner_image ?? 'default-banner.jpg' }}" alt="">
+                <div class="pj-banner__wrapper custom-wrapper">
+                    <div class="container">
+                        <div class="pj-banner__subtitle text-start">Dự án</div>
+                        <div class="pj-banner__title text-start">{{ $project->name }}</div>
+                        <div class="d-flex justify-content-end">
+                            <div class="custom_desc">{{ $project->short_desc ?? '' }}</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         @endif
         <nav class="project-nav">
             <div class="container">
@@ -81,8 +81,8 @@
                 </ul>
             </div>
         </nav>
-        <section class="section" id="thong-tin-chung"><img class="section__bg" src="{{asset('./images/achitect-bg.png')}}"
-            alt="">
+        <section class="section" id="thong-tin-chung"><img class="section__bg" src="{{ asset('./images/achitect-bg.png') }}"
+                alt="">
             <div class="container">
                 <h2 class="section__title">Thông tin chung</h2>
                 <div class="mx-auto" style="max-width: 800px;">
@@ -92,7 +92,8 @@
         </section>
         <section class="section pb-0" id="vi-tri">
             <div class="container">
-                <h2 class="section__title">Vị trí</h2><img class="w-100" src="{{asset('./images/position.jpg')}}" alt="">
+                <h2 class="section__title">Vị trí</h2><img class="w-100" src="{{ $project->location_image ?? asset('./images/position.jpg') }}"
+                    alt="">
             </div>
         </section>
         <section class="section section--light-blue" id="loi-the-noi-bat">
@@ -100,22 +101,27 @@
                 <h2 class="section__title">Lợi thế nổi bật</h2>
                 <div>
                     @php
-                        $images = explode(';', $project->advantage_images ?? '');
-                        $titles = json_decode($project->advantage_titles ?? '[]', true);
-                        $descs  = json_decode($project->advantage_descriptions ?? '[]', true);
+                        $images = $project->advantage_images ? explode(';', $project->advantage_images) : [];
+                        $titles = $project->advantage_titles ? json_decode($project->advantage_titles, true) : [];
+                        $descs = $project->advantage_descriptions
+                            ? json_decode($project->advantage_descriptions, true)
+                            : [];
+
+                        // Lấy giá trị min của 3 mảng
                         $count = min(count($images), count($titles), count($descs));
                     @endphp
+
                     @for ($i = 0; $i < $count; $i++)
                         <div class="advantage mt-20">
                             <a class="advantage__frame" href="#!">
-                                <img src="{{ $images[$i] }}" alt="" />
+                                <img src="{{ $images[$i] ?? '' }}" alt="" />
                             </a>
                             <div class="advantage__body">
                                 <div class="advantage__index">{{ $i + 1 }}</div>
                                 <div class="advantage__index-bg">{{ $i + 1 }}</div>
-                                <div class="advantage__title">{{ $titles[$i] }}</div>
+                                <div class="advantage__title">{{ $titles[$i] ?? '' }}</div>
                                 <div class="advantage__desc text-justify">
-                                    {!! $descs[$i] !!}
+                                    {!! $descs[$i] ?? '' !!}
                                 </div>
                             </div>
                         </div>
@@ -128,14 +134,16 @@
                 <div class="container">
                     <h2 class="section__title text-white">Sa bàn ảo</h2>
                     <div class="mt-3">
-                        <a href="{{$project->link_vrtour}}" class="btn btn-warning text-white custom-btn-vrtour" target="_blank" rel="noopener noreferrer">
+                        <a href="{{ $project->link_vrtour }}" class="btn btn-warning text-white custom-btn-vrtour"
+                            target="_blank" rel="noopener noreferrer">
                             Xem VR Tour
                         </a>
                     </div>
                 </div>
             </div>
             <div class="ratio ratio-2x1">
-                <iframe src="{{$project->link_sand_table}}" frameborder="0" allowfullscreen allow="fullscreen"></iframe>
+                <iframe src="{{ $project->link_sand_table }}" frameborder="0" allowfullscreen
+                    allow="fullscreen"></iframe>
             </div>
             <div class="container">
                 <h2 class="section__title text-white">Sa bàn ảo</h2>
@@ -144,22 +152,26 @@
         <section class="section" id="thiet-ke-va-mat-bang">
             <div class="container">
                 <h2 class="section__title">Thiết kế & mặt bằng</h2>
-                <div class="section__desc">{{$project->design_short_desc}}</div>
+                <div class="section__desc">{{ $project->design_short_desc }}</div>
                 <div class="design-slider">
                     <div class="design-slider__container swiper-container">
                         <div class="swiper-wrapper">
                             @php
-                            $design_images = explode(';', $project->design_images ?? '');
-                            $design_descs = json_decode($project->design_description ?? '[]', true);
-                            $design_count = min(count($design_images), count($design_descs));
+                                $design_images = $project->design_images ? explode(';', $project->design_images) : [];
+                                $design_descs = $project->design_description
+                                    ? json_decode($project->design_description, true)
+                                    : [];
+                                $design_count = min(count($design_images), count($design_descs));
                             @endphp
                             @for ($i = 0; $i < $design_count; $i++)
-                            <div class="design-slider__slide swiper-slide">
-                                <div class="design-slider__frame"><img src="{{$design_images[$i]}}" alt="" /></div>
-                                <div class="design-slider__overlay">
-                                    <div class="design-slider__content">{{$design_descs[$i]}}</div>
+                                <div class="design-slider__slide swiper-slide">
+                                    <div class="design-slider__frame">
+                                        <img src="{{ $design_images[$i] ?? '' }}" alt="" />
+                                    </div>
+                                    <div class="design-slider__overlay">
+                                        <div class="design-slider__content">{{ $design_descs[$i] ?? '' }}</div>
+                                    </div>
                                 </div>
-                            </div>
                             @endfor
                         </div>
                     </div>
@@ -171,7 +183,7 @@
                         <div class="swiper-wrapper">
                             @for ($i = 0; $i < $design_count; $i++)
                                 <div class="swiper-slide">
-                                    <div class="design-thumb-slider__frame"><img src="{{$design_images[$i]}}"
+                                    <div class="design-thumb-slider__frame"><img src="{{ $design_images[$i] }}"
                                             alt="" /></div>
                                 </div>
                             @endfor
@@ -183,7 +195,7 @@
         <section class="section section--light-blue" id="phap-ly">
             <div class="container">
                 <h2 class="section__title">Văn bản pháp quy</h2>
-                <div class="section__desc">{{$project->legal_short_desc}}</div>
+                <div class="section__desc">{{ $project->legal_short_desc }}</div>
                 <div class="legal-grid">
                     @php
                         $legal_descs = json_decode($project->legal_description, true);
@@ -192,7 +204,7 @@
                     @endphp
                     @for ($i = 0; $i < $legal_count; $i++)
                         <a class="legal" href="#!"><img class="legal__icon"
-                                src="{{asset('./images/icon-pdf.svg')}}" alt="" />
+                                src="{{ asset('./images/icon-pdf.svg') }}" alt="" />
                             <div class="legal__body">
                                 <div class="legal__title">{{ $legal_descs[$i] ?? 'Văn bản pháp quy' }}</div>
                             </div>
@@ -203,8 +215,8 @@
                 </nav>
             </div>
         </section>
-        <section class="section" id="tin-tuc"><img class="texture-1" src="{{asset('./images/texture-1.png')}}" alt=""><img
-                class="texture-2" src="{{asset('./images/texture-2.png')}}" alt="">
+        <section class="section" id="tin-tuc"><img class="texture-1" src="{{ asset('./images/texture-1.png') }}"
+                alt=""><img class="texture-2" src="{{ asset('./images/texture-2.png') }}" alt="">
             <div class="container">
                 <h2 class="section__title">Tin tức</h2>
                 <div class="news-slider">
@@ -217,7 +229,7 @@
                             @foreach ($posts as $item)
                                 <div class="swiper-slide">
                                     <div class="news"><a class="news__frame"
-                                            href="{{ route('post_detail',['id' => $item->id, 'slug' => $item->slug]) }}"><img
+                                            href="{{ route('post_detail', ['id' => $item->id, 'slug' => $item->slug]) }}"><img
                                                 src="{{ $item->image }}" alt="" /></a>
                                         <div class="news__body">
                                             <div class="news__info">
@@ -227,7 +239,7 @@
                                                 <div class="news__like"><i class="fal fa-fw fa-heart"></i></div>
                                             </div>
                                             <h3 class="news__title  custom-desc"><a
-                                                    href="{{ route('post_detail',['id' => $item->id, 'slug' => $item->slug]) }}">{{ $item->name }}</a>
+                                                    href="{{ route('post_detail', ['id' => $item->id, 'slug' => $item->slug]) }}">{{ $item->name }}</a>
                                             </h3>
                                             <div class="news__desc">{{ $item->description }}</div>
                                         </div>
