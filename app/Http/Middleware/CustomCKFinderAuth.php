9d3da3aba3b9610122f7ce06091de006e52e6bfc
Nguyen Thanh Tung <tung19022003@gmail.com>
2025-11-27 10:38:18 +0700

    permission

--- app/Http/Middleware/CustomCKFinderAuth.php
+++ app/Http/Middleware/CustomCKFinderAuth.php
@@ -5,19 +5,42 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Helper\CKFinder as CKFinderHelper;
 
 class CustomCKFinderAuth
 {
-    /**
+ /**
      * Handle an incoming request.
      *
-     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        config(['ckfinder.authentication' => function () {
-            return true;
+        # Set the authentication configuration for CKFinder
+        config(['ckfinder.authentication' => function() {
+            return Auth::check();
         }]);
+        # Configure the private folders for the user
+        if (Auth::check()) {
+            $this->configureUserPrivateFolders(Auth::user()->id);
+        }
         return $next($request);
+    }
+
+    /**
+     * Configure CKFinder private folders per user.
+     *
+     * @param string $userDirectory
+     * @return void
+     */
+    protected function configureUserPrivateFolders(string $userDirectory): void
+    {
+        $ckfinder = new CKFinderHelper($userDirectory);
+        $config   = $ckfinder->getConfiguration();
+        config([
+            'ckfinder.privateDir'       => $config['privateDir'],
+            'ckfinder.backends.default' => $config['backends'],
+        ]);
     }
 }
--- routes/backend.php
+++ routes/backend.php
@@ -199,3 +199,8 @@ Route::localized(function () {
         });
     });
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::any('/ckfinder/connector', '\CKSource\CKFinderBridge\Controller\CKFinderController@requestAction')->name('ckfinder_connector');
+    Route::any('/ckfinder/browser', '\CKSource\CKFinderBridge\Controller\CKFinderController@browserAction')->name('ckfinder_browser');
+});
