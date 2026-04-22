13f991b702e99baa0b05d5d08db78f8acedc15c3
Nguyen Thanh Tung <tung19022003@gmail.com>
2026-04-22 15:02:23 +0700

    refactor code

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -928,6 +928,7 @@
     <link href="https://unpkg.com/maplibre-gl@3.6.2/dist/maplibre-gl.css" rel="stylesheet" />
     <script src="https://unpkg.com/maplibre-gl@3.6.2/dist/maplibre-gl.js"></script>
     <script src="https://unpkg.com/@maplibre/maplibre-gl-leaflet@0.0.21/leaflet-maplibre-gl.js"></script>
+    <script src="/js/industry-styles.js"></script>
     <script src="/js/boundaries.js"></script>
     <script src="/js/railways.js"></script>
     <script src="/js/railway_stations.js"></script>
@@ -2107,73 +2108,6 @@
         }
 
         function createMarker(loc) {
-            const industryStyles = {
-                1: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/bridge.png"
-                },
-                2: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/anchor.png"
-                },
-                3: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/enviroment.png"
-                },
-                4: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/cityscape.png"
-                },
-                5: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/finance.png"
-                },
-                6: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/industrial.png"
-                },
-                7: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/train.png"
-                },
-                8: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/tourism.png"
-                },
-                9: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/planting.png"
-                },
-                10: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/technology.png"
-                },
-                11: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/education.png"
-                },
-                12: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/bus.png"
-                },
-                13: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/nature.png"
-                },
-                14: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/healthcare.png"
-                },
-                15: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/united.png"
-                },
-                16: {
-                    color: "#2a84d0",
-                    icon: "/images/custom-icon-map/artificial-intelligence.png"
-                },
-            };
-
             const style = industryStyles[loc.industry_number];
 
             let marker;
